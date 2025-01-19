32b9abf8866138b2cafbde2414323827657c9a45
Suryaansh Rai <suryaanshrai@gmail.com>
2025-01-19 15:44:37 +0530

    Containerised teh app for dev

--- ui/app/vite.config.ts
+++ ui/app/vite.config.ts
@@ -1,12 +1,23 @@
 import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 5173,
+  strictPort: true,
+ },
+ server: {
+  port: 5173,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:5173",
+ },
+ resolve: {
+  alias: {
+    "@": path.resolve(__dirname, "./src"),
   },
-})
+},
+});
